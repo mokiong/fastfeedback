8aef9f9a33c867d7f23bbdf33c6c91e7f5690f5b
mokiong <mokiong1427@gmail.com>
2021-02-11 19:00:52 +0800

    added github auth

---
+++ lib/auth.tsx
@@ -0,0 +1,54 @@
+import firebase from './firebase';
+import React, { useState, useEffect, useContext, createContext } from 'react';
+
+const AuthContext = createContext();
+
+export function ProvideAuth({ children }) {
+   const auth = useProvideAuth();
+   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => {
+   return useContext(AuthContext);
+};
+
+function useProvideAuth(): authCont {
+   const [user, setUser] = useState(null);
+   console.log(user);
+   const signinWithGithub = () => {
+      return firebase
+         .auth()
+         .signInWithPopup(new firebase.auth.GithubAuthProvider())
+         .then((response) => {
+            setUser(response.user);
+            return response.user;
+         });
+   };
+
+   const signout = () => {
+      return firebase
+         .auth()
+         .signOut()
+         .then(() => {
+            setUser(false);
+         });
+   };
+
+   useEffect(() => {
+      const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+         if (user) {
+            setUser(user);
+         } else {
+            setUser(false);
+         }
+      });
+
+      return () => unsubscribe();
+   }, []);
+
+   return {
+      user,
+      signinWithGithub,
+      signout,
+   };
+}
