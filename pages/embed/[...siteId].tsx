822a0d7e5dec37f0b69dbdb85ecb9220db153525
mokiong <mokiong1427@gmail.com>
2021-02-28 18:30:22 +0800

    updated

--- pages/embed/[...siteId].tsx
+++ pages/embed/[...siteId].tsx
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
    const { sites } = await getAllSites();
    const paths = sites.map((site) => ({
       params: {
-         site: [site.id.toString()],
+         site: site.id.toString(),
       },
    }));
 
